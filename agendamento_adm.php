3074468006974d86a6f5ce8e9690f19367d08022
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2026-03-18 16:50:05 -0300

    SOCORRO

--- agendamento_adm.php
+++ agendamento_adm.php
@@ -14,7 +14,8 @@ if (!$result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consultas Agendadas - ADM</title>
-    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="style6.css">
+    <link rel="shortcut icon" href="imagens/logo.png">
 </head>
 <body>
 
--- meus_agendamentos.php
+++ meus_agendamentos.php
@@ -20,7 +20,8 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meus Agendamentos</title>
-    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="style6.css">
+    <link rel="shortcut icon" href="imagens/logo.png">
 </head>
 <body>
 
